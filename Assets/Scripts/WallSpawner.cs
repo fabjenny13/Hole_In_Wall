3b37a197e78dea9c573f309077772523bb5868ad
fabjenny13 <65107441+fabjenny13@users.noreply.github.com>
2026-02-09 23:50:48 +0530

    can create custom wall with random hole

--- Assets/Scripts/WallSpawner.cs
+++ Assets/Scripts/WallSpawner.cs
@@ -5,18 +5,47 @@ public class WallSpawner : MonoBehaviour
 {
     public GameObject wallPrefab;
     public Vector3 spawnLocation;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    public GameObject wallPiece;
+    int height, width; //no. of blocks across and down
     void Start()
     {
-        StartCoroutine(SpawnWall());
+        //StartCoroutine(SpawnWall());
+        height = 9;
+        width = 9;
+
+        CreateWall();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateWall()
     {
-        
-    }
 
+        GameObject parentWall = new GameObject();
+        parentWall.transform.position = spawnLocation;
+
+        int skipH = Random.Range(1, height - 2);
+        int skipW = Random.Range(1, width - 2);
+
+        int upperLimit = height / 2;
+        int leftLimit = width / 2;
+
+        for(int i = 0; i < height; i++)
+        {
+            for(int j = 0; j < width; j++)
+            {
+                if (!(i == skipH && j == skipW))
+                {
+                    Vector3 loc = new Vector3(leftLimit, upperLimit, parentWall.transform.position.z);
+                    Instantiate(wallPiece, loc, Quaternion.Euler(0, 0, 0), parentWall.transform);
+                }
+                leftLimit++;
+            }
+            leftLimit = width / 2;
+            upperLimit++;
+        }
+    }
+    
     IEnumerator SpawnWall()
     {
         while (true)
