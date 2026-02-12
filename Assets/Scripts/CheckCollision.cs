c96457df343feee3889cb4b507e3a23d5874d9b7
fabjenny13 <65107441+fabjenny13@users.noreply.github.com>
2026-02-12 22:27:16 +0530

    fix: player can't crash into multiple wall pieces

--- Assets/Scripts/CheckCollision.cs
+++ Assets/Scripts/CheckCollision.cs
@@ -1,15 +1,23 @@
+using System;
 using UnityEngine;
 
 public class CheckCollision : MonoBehaviour
 {
-
+    bool hasCrashed = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        Debug.Log("Crashed");
+        if(other.tag == "WallPiece" && !hasCrashed)
         {
-            GetComponent<Animator>().SetTrigger("Destroy");
+            hasCrashed = true;
+            other.GetComponent<Animator>().SetTrigger("Destroy");
             FindFirstObjectByType<UIManager>().DecreaseScore();
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        hasCrashed = false;
     }
 }
